0bc85ea6f147ad113e229a6a6e1f40407874d137
bored_dam <damianotrovato122@gmail.com>
2025-11-08 14:00:49 +0100

    first commit

---
+++ BoredQiskit/FastQiskit.py
@@ -0,0 +1,59 @@
+from qiskit_aer import AerSimulator
+from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
+from qiskit_ibm_runtime import SamplerV2 as Sampler
+from qiskit_ibm_runtime.fake_provider import FakeVigoV2, FakeTorino, FakeGeneva, FakeBrisbane
+
+def run(qc, shots):
+    sim_backend = AerSimulator()
+    pm = generate_preset_pass_manager(backend=sim_backend)
+    isa_qc = pm.run(qc)
+    sampler = Sampler(mode=sim_backend)
+
+    job = sampler.run([isa_qc], shots=shots)
+    results = job.result()
+    counts = results[0].data.c.get_counts()
+    return counts
+
+def vigo_run(qc, shots):
+    sim_backend = FakeVigoV2()
+    pm = generate_preset_pass_manager(backend=sim_backend)
+    isa_qc = pm.run(qc)
+    sampler = Sampler(mode=sim_backend)
+
+    job = sampler.run([isa_qc], shots=shots)
+    results = job.result()
+    counts = results[0].data.c.get_counts()
+    return counts
+
+def torino_run(qc, shots):
+    sim_backend = FakeTorino()
+    pm = generate_preset_pass_manager(backend=sim_backend)
+    isa_qc = pm.run(qc)
+    sampler = Sampler(mode=sim_backend)
+
+    job = sampler.run([isa_qc], shots=shots)
+    results = job.result()
+    counts = results[0].data.c.get_counts()
+    return counts
+
+def geneva_run(qc, shots):
+    sim_backend = FakeGeneva()
+    pm = generate_preset_pass_manager(backend=sim_backend)
+    isa_qc = pm.run(qc)
+    sampler = Sampler(mode=sim_backend)
+
+    job = sampler.run([isa_qc], shots=shots)
+    results = job.result()
+    counts = results[0].data.c.get_counts()
+    return counts
+
+def brisbane_run(qc, shots):
+    sim_backend = FakeBrisbane()
+    pm = generate_preset_pass_manager(backend=sim_backend)
+    isa_qc = pm.run(qc)
+    sampler = Sampler(mode=sim_backend)
+
+    job = sampler.run([isa_qc], shots=shots)
+    results = job.result()
+    counts = results[0].data.c.get_counts()
+    return counts
---
+++ README.md
@@ -0,0 +1,15 @@
+# Quantum Algorithms
+
+This repository will contain some algorithms, protocols and experiments related to **quantum computing**.
+
+### Repo structure
+
+```py
+quantum-algorithms
+├── BoredQiskit         # package for easier qiskit usage
+│   ├── __init__.py
+│   └── FastQiskit.py   # module for simulation on fake Q-hardware
+├── algorithm-A
+├── ...
+└── algorithm-Z
+```
