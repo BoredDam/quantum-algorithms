07d7aeed38b55b2b1ca57b1fd48885d31a534bb5
bored_dam <damianotrovato122@gmail.com>
2025-12-11 19:22:12 +0100

    feat: added more settings for printing simulations results

--- BoredQiskit/FastQiskit.py
+++ BoredQiskit/FastQiskit.py
@@ -3,10 +3,11 @@ from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
 from qiskit_ibm_runtime import SamplerV2 as Sampler
 from qiskit_ibm_runtime.fake_provider import FakeVigoV2, FakeTorino, FakeGeneva, FakeBrisbane
 
-def run(qc, shots=1024):
+def run(qc, shots=1024, more_info=False):
     """
     runs the job results for a `qc` QuantumCircuit on the ideal simulator AerSimulator. 
-    `shots` lets you specify how many times should the simulation run.
+    `shots` lets you specify how many times should the simulation run. if `more_info` is set to True,
+    it will return a dict sorted by 
     """
     sim_backend = AerSimulator()
     pm = generate_preset_pass_manager(backend=sim_backend)
@@ -15,7 +16,20 @@ def run(qc, shots=1024):
 
     job = sampler.run([isa_qc], shots=shots)
     results = job.result()
-    counts = results[0].data.c.get_counts()
+    data = results[0].data
+
+    if (more_info):
+        counts = {}
+        for name in dir(data):
+            if name.startswith("_"):
+                continue
+            reg_obj = getattr(data, name)
+            if hasattr(reg_obj, "get_counts"):
+                counts[name] = reg_obj.get_counts()
+    else:
+        reg_name = list(data.__dict__.keys())[0]
+        counts = getattr(data, reg_name).get_counts()
+        counts = results[0].data.c.get_counts()
     return counts
 
 def vigo_run(qc, shots=1024):
